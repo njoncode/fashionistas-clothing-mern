399bd3d894a6b29cc93390113f532a85f2670dd9
njoncode <njoncode@gmail.com>
2021-09-19 14:11:30 +0530

    Debugging Our Code

--- src/components/ShopPage.js
+++ src/components/ShopPage.js
@@ -8,7 +8,7 @@ import CollectionPage from './CollectionPage';
 import WithSpinner from './WithSpinner';
 
 import { fetchCollectionsStartAsync } from '../redux/shop/shop.actions';
-import { selectIsCollectionFetching}  from '../redux/shop/shop.selectors';
+import { selectIsCollectionFetching, selectIsCollectionsLoaded}  from '../redux/shop/shop.selectors';
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
@@ -21,19 +21,20 @@ class ShopPage extends React.Component {
     };
 
     render() {
-        const { match, isCollectionFetching} = this.props;
+        const { match, isFetchingCollections, isCollectionsLoaded} = this.props;
 
         return (
             <div className='shope-page'>
-                <Route exact path={match.path} render={(props) => <CollectionsOverviewWithSpinner isLoading={isCollectionFetching} {...props} />}/>
-                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props} />}/>
+                <Route exact path={match.path} render={(props) => <CollectionsOverviewWithSpinner isLoading={isFetchingCollections} {...props} />}/>
+                <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props} />}/>
             </div>
         );
     }
 }
 
 const mapStateToProps = createStructuredSelector({
-    isCollectionFetching: selectIsCollectionFetching
+    isFetchingCollections: selectIsCollectionFetching,
+    isCollectionsLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -25,6 +25,12 @@ export const selectIsCollectionFetching = createSelector(
     [selectShop],
     shop =>  shop.isFetching
 );
+
+export const selectIsCollectionsLoaded = createSelector(
+    [selectShop],
+    shop =>  !!shop.collections
+);
+    
     
 
 
